2bb01959782c0beb69ae83b7418c4610849c1401
Laud Gilbert <laudaccurate@gmail.com>
2021-12-20 17:35:46 +0000

    finished route handlers

--- server/routes.js
+++ server/routes.js
@@ -23,4 +23,47 @@ module.exports = (app) => {
       console.log(error.message);
     }
   });
+
+  app.get("/todos/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+        id,
+      ]);
+
+      res.json(todo.rows[0]);
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
+
+  app.put("/todos/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { description } = req.body;
+
+      const updatedTodo = await pool.query(
+        "UPDATE todo SET description = $1 WHERE todo_id = $2",
+        [description, id]
+      );
+
+      res.json("Todo successfully updated");
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
+
+  app.delete("/todos/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const deleteTodo = await pool.query(
+        "DELETE FROM todo WHERE todo_id = $1",
+        [id]
+      );
+
+      res.json("Todo was successfully deleted");
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
 };
